bc9113f7c9e8af1b3b15cf04924224a71e659259
Sif Et tabaa <sifbusinesswork@gmail.com>
2023-02-02 20:12:17 +0100

    cta added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import CTA from "./CTA/CTA";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="container container__header">
+      <h5>Hello, I'm</h5>
+      <h1>Sif Et tabaa</h1>
+      <h5 className="text-light">FullStack Developer</h5>
+      <CTA/>
 
-export default Header
+    </header>
+  );
+};
+
+export default Header;
